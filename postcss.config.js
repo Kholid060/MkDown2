41f91faa2fdf726ef46150cc105cb361319f1fa2
Ahmad Kholid <kholid060@gmail.com>
2020-04-16 20:18:20 +0800

    add whitelist

--- postcss.config.js
+++ postcss.config.js
@@ -3,7 +3,7 @@ module.exports = {
     tailwindcss: {},
     'vue-cli-plugin-tailwind/purgecss': {
       whitelist: ['tooltip-ui'],
-      whitelistPatterns: [/CodeMirror/, /simplebar/, /hljs/, /markdown-content/, /cm-s-ayu-mirage/, /cm/],
+      whitelistPatterns: [/CodeMirror/, /simplebar/, /hljs/, /markdown-content/, /(cm-s-ayu-mirage|cm)/, /vue-popover-theme/],
       whitelistPatternsChildren: [/(table|img|tr|td|tbody|thead)/],
     },
   },
