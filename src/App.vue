a5857b4e6f197ad41299194b463c335e3fd019f0
Ahmad Kholid <kholid060@gmail.com>
2020-04-16 16:55:27 +0800

    fix content resize

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,30 @@
 <template>
   <div class="app">
-    <router-view />
+    <router-view v-if="retrieved" />
+    <div
+     class="h-full w-full absolute splash"
+     v-else>
+      <div class="content">
+        <h1 class="text-6xl font-bold">MkDown</h1>
+        <p class="text-2xl text-lighter">Online Markdown Editor</p>
+        <p class="text-lighter mt-6">Loading...</p>
+      </div>
+    </div>
   </div>
 </template>
+<script>
+export default {
+  data: () => ({
+    retrieved: false,
+  }),
+  created() {
+    if (this.$route.name === 'Home') {
+      this.$store.dispatch('retrieve').then(() => {
+        this.retrieved = true;
+      });
+    } else {
+      this.retrieved = true;
+    }
+  },
+};
+</script>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,36 +1,26 @@
 <template>
   <div class="flex">
-    <template v-if="retrieved">
-      <side-menu v-if="sideMenu" @close="sideMenu = false"></side-menu>
-      <div :style="{ width: contentWidth }">
-        <Menu v-model="sideMenu"></Menu>
-        <div class="flex" style="height: calc(100vh - 4rem)">
-          <button-ui
-           icon
-           type="primary"
-           round
-           @click="preview = !preview"
-           class="absolute lg:hidden shadow-xl z-20 bottom-0 right-0 m-6">
-            <v-mdi size="28" name="mdi-eye"></v-mdi>
-          </button-ui>
-          <div class="bg-light lg:w-6/12 w-screen" v-show="!preview">
-            <vue-codemirror v-model="markdown"></vue-codemirror>
-          </div>
-          <simplebar
-           data-simplebar-auto-hide="false"
-           class="lg:w-6/12 px-6 py-3 panel2 h-full hidden w-full"
-           :class="{'active-preview': preview}">
-            <preview-content :content="markdown"></preview-content>
-          </simplebar>
+    <side-menu v-if="sideMenu" @close="sideMenu = false"></side-menu>
+    <div :style="{ width: contentWidth }">
+      <Menu v-model="sideMenu"></Menu>
+      <div class="flex" style="height: calc(100vh - 4rem)">
+        <button-ui
+         icon
+         type="primary"
+         round
+         @click="preview = !preview"
+         class="absolute lg:hidden shadow-xl z-20 bottom-0 right-0 m-6">
+          <v-mdi size="28" name="mdi-eye"></v-mdi>
+        </button-ui>
+        <div class="bg-light lg:w-6/12 w-screen" v-show="!preview">
+          <vue-codemirror v-model="markdown"></vue-codemirror>
         </div>
-      </div>
-    </template>
-    <div
-     class="h-full w-full absolute splash"
-     v-else>
-      <div class="content">
-        <h1 class="text-6xl font-bold">MkDown</h1>
-        <p class="text-2xl text-lighter">Online Markdown Editor</p>
+        <simplebar
+         data-simplebar-auto-hide="false"
+         class="lg:w-6/12 px-6 py-3 panel2 h-full hidden w-full"
+         :class="{'active-preview': preview}">
+          <preview-content :content="markdown"></preview-content>
+        </simplebar>
       </div>
     </div>
   </div>
@@ -49,26 +39,32 @@ export default {
     VueCodemirror, PreviewContent, simplebar, Menu, SideMenu,
   },
   data: () => ({
+    contentWidth: '100%',
     markdown: '',
-    retrieved: false,
     sideMenu: false,
     preview: false,
   }),
-  computed: {
-    contentWidth() {
-      const width = this.sideMenu ? window.innerWidth - 280 : window.innerWidth;
-
-      return `${width}px`;
+  watch: {
+    sideMenu() {
+      this.resizeHandler();
     },
   },
+  methods: {
+    resizeHandler() {
+      const width = this.sideMenu ? window.innerWidth - 280 : window.innerWidth;
+      this.contentWidth = `${width}px`;
+    },
+  },
+  created() {
+    window.addEventListener('resize', this.resizeHandler);
+  },
   mounted() {
-    this.$store.dispatch('retrieve').then(() => {
-      this.retrieved = true;
-
-      setTimeout(() => {
-        syncScroll(['.CodeMirror-scroll', '.panel2 .simplebar-content-wrapper']);
-      }, 2000);
-    });
+    setTimeout(() => {
+      syncScroll(['.CodeMirror-scroll', '.panel2 .simplebar-content-wrapper']);
+    }, 2000);
+  },
+  destroyed() {
+    window.removeEventListener('resize', this.resizeHandler);
   },
 };
 </script>
